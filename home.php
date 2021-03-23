ca89dd5d02f46a1a900e03a4dcb0b81d128faf34
Dennis Kamau <dmkamau475@gmail.com>
2021-03-23 12:07:51 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP CRM</title>
+    <link rel="canonical" href=" { { http://localhost/phpcrm } }{ { home.php } }"/>
+    <!--    bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+<nav class="nav justify-content-between bg-light">
+    <span class="px-5"><a href="http://localhost/phpcrm" class="navbar-brand">PHP CRM</a></span>
+    <span class="d-inline-flex text-right">
+        <a class="nav-item nav-link" href="#"><span class="navigation-item">log In</span></a>
+        <a class="nav-item nav-link" href="#"><span class="navigation-item">Sign Up</span></a>
+    </span>
+</nav>
+
+
+<section>
+    <div class="container ml-auto mr-auto">
+        <div class="col-md-6">
+            <h1>Please Log in to proceed</h1>
+            <br/>
+            <form action="" method="post" class="form">
+                <div class="form-row">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" value="" class="form-control">
+                </div>
+                <div class="form-row">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <div class="form-row">
+                    <button type="submit" class="btn btn-success mt-2">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<!--Bootstrap-->
+<script src="/js/bootstrap.js"></script>
+</body>
+</html>
+
+
